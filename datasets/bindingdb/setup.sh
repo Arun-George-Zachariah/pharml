1ad6f853a7aa91a8cfd5cb72e5f41022b051257b
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-08-08 23:30:00 -0500

    Adding cpu support.

--- datasets/bindingdb/setup.sh
+++ datasets/bindingdb/setup.sh
@@ -18,3 +18,25 @@ mkdir data
 
 # Converting sdf to dataset.
 python sdf_to_dataset.py --sdf $HOME/BindingDB_All_terse_2D.sdf --out data --threads $NO_OF_THREADS
+
+# Configuring Open MPI
+wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.4.tar.gz -O $HOME/openmpi-4.0.4.tar.gz
+gunzip $HOME/openmpi-4.0.4.tar.gz
+tar -xvf $HOME/openmpi-4.0.4.tar --directory $HOME
+$HOME/openmpi-4.0.4/configure --prefix=/usr/local
+sudo make all install
+sudo ldconfig
+
+#Upgrading gcc and g++
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get -y install gcc-8 g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 20 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+
+# Installing Horovod
+pip install horovod
+
+# Moving directories (Hack to make mldock_gnn.py work.)
+cp -r data/lig ../../
+cp -r data/nhg ../../
